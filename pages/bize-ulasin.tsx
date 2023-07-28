328b25d050b9c21766bf5e924f4c8107c18cb66f
SemihCetin <semihcetin34@gmail.com>
2023-07-28 18:00:30 +0300

    Bize ulasın simple page maded

--- pages/bize-ulasin.tsx
+++ pages/bize-ulasin.tsx
@@ -1,89 +1,65 @@
 import React from "react";
-import { GetServerSideProps } from "next";
-import ReactMarkdown from "react-markdown";
 import Layout from "../components/Layout";
-import Router from "next/router";
 import { PostProps } from "../components/Post";
-import prisma from '../lib/prisma'
-import { useSession } from "next-auth/react";
-
-
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const post = await prisma.post.findUnique({
-    where: {
-      id: Number(params?.id) || -1,
-    },
-    include: {
-      author: {
-        select: { name: true, email: true },
-      },
-    },
-  });
-  return {
-    props: post,
-  };
-};
-
-async function publishPost(id: number): Promise<void> {
-  await fetch(`/api/publish/${id}`, {
-    method: "PUT",
-  });
-  await Router.push("/")
-}
-
-async function deletePost(id: number): Promise<void> {
-  await fetch(`/api/post/${id}`, {
-    method: "DELETE",
-  });
-  await Router.push("/")
-}
 
 const Post: React.FC<PostProps> = (props) => {
-  const { data: session, status } = useSession();
-  if (status === 'loading') {
-    return <div>Authenticating ...</div>;
-  }
-  const userHasValidSession = Boolean(session);
-  const postBelongsToUser = session?.user?.email === props.author?.email;
-  let title = props.title;
-  if (!props.published) {
-    title = `${title} (Draft)`;
-  }
-
   return (
     <Layout>
-      <div>
-        <h2>{title}</h2>
-        <p>By {props?.author?.name || "Unknown author"}</p>
-        <ReactMarkdown children={props.content} />
-        {!props.published && userHasValidSession && postBelongsToUser && (
-          <button onClick={() => publishPost(props.id)}>Publish</button>
-        )}
-        {userHasValidSession && postBelongsToUser && (
-          <button onClick={() => deletePost(props.id)}>Delete</button>
-        )}
-      </div>
-      <style jsx>{`
-        .page {
-          background: white;
-          padding: 2rem;
-        }
-
-        .actions {
-          margin-top: 2rem;
-        }
-
-        button {
-          background: #ececec;
-          border: 0;
-          border-radius: 0.125rem;
-          padding: 1rem 2rem;
-        }
-
-        button + button {
-          margin-left: 1rem;
-        }
-      `}</style>
+        <div className="page-title">
+            Gallery
+        </div>
+        <div className="grid-container">
+            <div className="card text-center">
+                <div className="icon-circle bg-gray">
+                    <i className="fa fa-envelope-o m-auto" aria-hidden="true"></i>
+                </div>
+                <p>Teklif Almak İstiyorum</p>
+                <button className="btn btn-gray" type="button">Bize Mail atın</button>
+            </div>
+            <div className="card text-center">
+                <div className="icon-circle bg-gray">
+                    <i className="fa fa-whatsapp" aria-hidden="true"></i>
+                </div>
+                <p>Bire bir iletişim kurmak istiyorum</p>
+                <button className="btn btn-gray" type="button">Whatsup Kanalımız</button>
+            </div>
+            <div className="card text-center">
+                <div className="icon-circle bg-gray">
+                    <i className="fa fa-instagram" aria-hidden="true"></i>
+                </div>
+                <p>Instagram Sayfamızı takip edebilirsiniz</p>
+                <button className="btn btn-gray" type="button">Instagram Kanalımız</button>
+            </div>
+            <div className="card text-center">
+                <div className="icon-circle bg-gray">
+                    <i className="fa fa-facebook-official" aria-hidden="true"></i>
+                </div>
+                <p>Facebook Sayfamızı takip edebilirsiniz</p>
+                <button className="btn btn-gray" type="button">Facebook Kanalımız</button>
+            </div>
+            <div className="card text-center">
+                <div className="icon-circle bg-gray">
+                    <i className="fa fa-volume-control-phone" aria-hidden="true"></i>
+                </div>
+                <p>0534 961 32 65</p>
+                <button className="btn btn-gray" type="button">Telefon Numaramız</button>
+            </div>
+            <div className="card text-center">
+                <div className="icon-circle bg-gray">
+                    <i className="fa fa-cart-arrow-down" aria-hidden="true"></i>
+                </div>
+                <p>Online Alışveriş</p>
+                <button className="btn btn-gray" type="button">Trendyol'da Ürünlerimiz</button>
+            </div>
+            <div className="card text-center">
+                <div className="icon-circle bg-gray">
+                    <i className="fa fa-cart-arrow-down" aria-hidden="true"></i>
+                </div>
+                <p>Online Alışveriş</p>
+                <button className="btn btn-gray" type="button">Hepsi Burada'da Ürünlerimiz</button>
+            </div>
+           
+        </div>
     </Layout>
   );
 };
