dd9121a1d281e2da5cb86aecfa3501826a8b97a9
SemihCetin <semihcetin34@gmail.com>
2023-07-28 17:59:55 +0300

    Main page added and card component

---
+++ components/Card.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Router from "next/router";
+import ReactMarkdown from "react-markdown";
+
+export type CardProps = {
+  id: number;
+  name: string;
+  image_path1: string;
+};
+
+const Card: React.FC<{ card: CardProps }> = ({ card }) => {
+   return (
+    <div className="card bg-gray text-center">
+      <img src={card.image_path1} alt=""/>
+      <p>{card.name}</p>
+      <button className="btn btn-gray" type="button">Özellikler</button>
+    </div>
+  )
+};
+
+export default Card;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,73 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap/dist/css/bootstrap.css'
 import type { GetServerSideProps } from "next";
 import Layout from "../components/Layout";
-import Post, { PostProps } from "../components/Post";
+import Card, { CardProps } from "../components/Card";
 import prisma from '../lib/prisma'
+import { Product, Color, Brand } from "@prisma/client";
+
+type ProductProps = {
+  colors: Array<Color>;
+  brands: Array<Brand>;
+  products: Array<Product>;
+};
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const feed = await prisma.post.findMany({
-    where: {
-      published: true,
-    },
+  const brands = await prisma.brand.findMany({});
+  const colors = await prisma.color.findMany({});
+  const products = await prisma.product.findMany({
     include: {
-      author: {
+      brand: {
         select: {
-          name: true,
+          id:true,
+          name: true
         },
       },
+      color: {
+        select: {
+          id:true,
+          name: true
+        }
+      }
     },
   });
+  const data:ProductProps ={
+    products,
+    colors,
+    brands
+  }
+  
   return {
-    props: { feed },
+    props: { ...data },
   };
 };
 
-type Props = {
-  feed: PostProps[];
-};
 
-const Blog: React.FC<Props> = (props) => {
+const Blog: React.FC<ProductProps> = (props) => {
   return (
     <Layout>
-      <div className="page">
-        <h1>Public Feed</h1>
-        <main>
-          {props.feed.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
-            </div>
-          ))}
-        </main>
+      <div className="page-title">Ürünler</div>
+      <div>
+            <select name="colors">
+            <option value="">Tüm Renkler</option>
+              {props.colors.map((c) => 
+                 <option value={c.id}>{c.name}</option>
+              )}
+            </select>
+            <select name="brands">
+            <option value="">Tüm Markalar</option>
+              {props.brands.map((b) => 
+                 <option value={b.id}>{b.name}</option>
+              )}
+            </select>
+            <button className="btn btn-gray"> Getir <i className="fa fa-search" aria-hidden="true"></i> </button>
+        </div>
+      <div className="grid-container">
+        {props.products.map((p) => (
+            <Card card={p}/>
+        ))}
+            
       </div>
-      <style jsx>{`
-        .post {
-          background: white;
-          transition: box-shadow 0.1s ease-in;
-        }
-
-        .post:hover {
-          box-shadow: 1px 1px 3px #aaa;
-        }
-
-        .post + .post {
-          margin-top: 2rem;
-        }
-      `}</style>
     </Layout>
   );
 };
