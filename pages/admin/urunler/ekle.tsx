a5233dbb416443abde5fda8002ad353025506d33
SemihCetin <semihcetin34@gmail.com>
2023-07-31 21:50:37 +0300

    Ekle page edited and tested

--- pages/admin/urunler/ekle.tsx
+++ pages/admin/urunler/ekle.tsx
@@ -4,79 +4,81 @@ import axios from "axios";
 import fs from "fs/promises";
 import path from "path";
 import Link from "next/link";
+import Layout from "../../../components/Layout";
+import Breadcrumb from "../../../components/Breadcrumb";
+import prisma from "../../../lib/prisma";
+import { Brand, Color } from "@prisma/client";
 
-interface Props {
-  dirs: string[];
+type Props = {
+  Brands: Brand[];
+  Colors: Color[];
 }
 
-const Home: NextPage<Props> = ({ dirs }) => {
-  const [uploading, setUploading] = useState(false);
-  const [selectedImage, setSelectedImage] = useState("");
-  const [selectedFile, setSelectedFile] = useState<File>();
+export const getServerSideProps: GetServerSideProps = async () => {
+  const props: Props = {Brands:[],Colors:[]};
+  try {
+    const brands = await prisma.brand.findMany({});
+    const colors = await prisma.color.findMany({});
+    props.Brands=brands;
+    props.Colors=colors;
+    return {props}
+  } catch (error) {
+    return {props};
+  }
+}
 
-  const handleUpload = async () => {
-    setUploading(true);
+const Home: NextPage<{props:Props}> = ( props ) => {
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    
     try {
-      if (!selectedFile) return;
-      const formData = new FormData();
-      formData.append("myImage", selectedFile);
-      const { data } = await axios.post("/api/image", formData);
-      console.log(data);
-    } catch (error: any) {
-      console.log(error.response?.data);
+      const name=document.getElementById('name').value;
+      const colorId=document.getElementById('colorId').value;
+      const brandId=document.getElementById('brandId').value;
+      const result = await axios.post('/api/admin/product',{
+        name, colorId, brandId
+      })
+        console.log('result',result);
+    } catch (err) {
+        console.log(err);
     }
-    setUploading(false);
-  };
-
+  }
   return (
-    <div className="max-w-4xl mx-auto p-20 space-y-6">
-      <label>
-        <input
-          type="file"
-          hidden
-          onChange={({ target }) => {
-            if (target.files) {
-              const file = target.files[0];
-              setSelectedImage(URL.createObjectURL(file));
-              setSelectedFile(file);
-            }
-          }}
-        />
-        <div className="w-40 aspect-video rounded flex items-center justify-center border-2 border-dashed cursor-pointer">
-          {selectedImage ? (
-            <img src={selectedImage} alt="" />
-          ) : (
-            <span>Select Image</span>
-          )}
-        </div>
-      </label>
-      <button
-        onClick={handleUpload}
-        disabled={uploading}
-        style={{ opacity: uploading ? ".5" : "1" }}
-        className="bg-red-600 p-3 w-32 text-center rounded text-white"
-      >
-        {uploading ? "Uploading.." : "Upload"}
-      </button>
-      <div className="mt-20 flex flex-col space-y-3">
-        {dirs.map((item) => (
-          <Link key={item} href={"/images/" + item}>
-            <a className="text-blue-500 hover:underline">{item}</a>
-          </Link>
-        ))}
+    <Layout>
+      <div className="page-title">
+        <Breadcrumb items={[{text:"Admin", link:"/admin"}, {text:"Urunler", link:"/admin/urunler"}, {text: "Ürün Ekle"}]}/>
       </div>
-    </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+        <div className="form__group field">
+          <input type="input" className="form__field" placeholder="Name" name="name" id='name' required />
+          <label htmlFor={"name"} className="form__label">Ürün Adı</label>
+        </div>
+        <div className="form__group field">
+           <select name="colorId" id="colorId">
+            <option value="">Tüm Renkler</option>
+              {props.Colors.map((c) => 
+                 <option key={c.id} value={c.id}>{c.name}</option>
+              )}
+            </select>
+        </div>
+        <div className="form__group field">
+           <select name="brandId" id="brandId">
+            <option value="">Tüm Markalar</option>
+              {props.Brands?.map((b) => 
+                 <option key={b.id} value={b.id}>{b.name}</option>
+              )}
+            </select>
+        </div>
+        <div className="form__group field">
+          <button type="submit" className="btn btn-green">Ekle <i className="fa fa-plus" aria-hidden="true"></i></button>  
+        </div>
+        </form>
+
+      </div>
+    </Layout>
   );
 };
-export const getServerSideProps: GetServerSideProps = async () => {
-  const props = { dirs: [] };
-  try {
-    const dirs = await fs.readdir(path.join(process.cwd(), "/public/images"));
-    props.dirs = dirs as any;
-    return { props };
-  } catch (error) {
-    return { props };
-  }
-};
+
 
 export default Home;
