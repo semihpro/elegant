327a40383d16e6611874955cc173e1014be92e28
SemihCetin <semihcetin34@gmail.com>
2023-08-10 23:45:22 +0300

    image upload api successfull

--- pages/api/admin/product/images/[id].ts
+++ pages/api/admin/product/images/[id].ts
@@ -1,19 +1,77 @@
-import { NextApiHandler } from 'next';
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiHandler,NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../../../../lib/prisma'
 import { getSession } from 'next-auth/react'
+import formidable from "formidable";
+import path from "path";
+import fs from "fs/promises";
+import { useRouter } from 'next/router';
+import { Product } from '@prisma/client';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+//test deployment
+const readFile = (
+  req: NextApiRequest,
+  saveLocally?: boolean,
+  folderAdress?: string
+): Promise<{ fields: formidable.Fields; files: formidable.Files }> => {
+  const options: formidable.Options = {};
+  //const fullAddress = 
+  if (saveLocally) {
+    options.uploadDir =folderAdress;
+    options.filename = (name, ext, path, form) => {
+      return Date.now().toString() + "_" + path.originalFilename;
+    };
+  }
+  options.maxFileSize = 4000 * 1024 * 1024;
+  const form = formidable(options);
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) reject(err);
+      resolve({ fields, files });
+    });
+  });
+};
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {  
-  const {image_path1, image_path2, image_path3, image_path4, image_path5} = req.body;
-  if(req.method==='PUT'){
-    const result = await prisma.product.update({ where:{id:Number(req.query?.id)},
-      data:{
-        image_path1 : !image_path1 ? null : String(image_path1),
-        image_path2 : !image_path2 ? null : String(image_path2),
-        image_path3 : !image_path3 ? null : String(image_path3),
-        image_path4 : !image_path4 ? null : String(image_path4),
-        image_path5 : !image_path5 ? null : String(image_path5)
-      }});
-    res.json(result); 
+  const {id,target} = req.query;
+  const product:Product = await prisma.product.findFirst({where:{id:Number(id)}});
+  if(!product) {
+    res.json({
+      result:`ok`,
+      status:200
+    })
   }
+  const folderAdress=path.join(process.cwd() + "/public/images/product/",`${id}`);
+  try {
+    await fs.readdir(folderAdress);
+  } catch (error) {
+    await fs.mkdir(folderAdress);
+  }
+  const result = await readFile(req, true, folderAdress);
+  // if(req.method==='PUT'){
+  //   const result = await prisma.product.update({ where:{id:Number(req.query?.id)},
+  //     data:{
+  //       image_path1 : !image_path1 ? null : String(image_path1),
+  //       image_path2 : !image_path2 ? null : String(image_path2),
+  //       image_path3 : !image_path3 ? null : String(image_path3),
+  //       image_path4 : !image_path4 ? null : String(image_path4),
+  //       image_path5 : !image_path5 ? null : String(image_path5)
+  //     }});
+  //   res.json(result); 
+  // }
+  let imageTarget = {};
+  imageTarget[target] = result.files.image[0].newFilename;
+  const updateResult = await prisma.product.update({where:{id:Number(id)},
+    data:{
+      ...imageTarget
+    }
+  })
+  res.json({
+    result:result,
+    status:200
+  })
 }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,51 +2,6 @@ import { PrismaClient, Prisma } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const userData: Prisma.UserCreateInput[] = [
-  {
-    name: 'Alice',
-    email: 'alice@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Join the Prisma Slack',
-          content: 'https://slack.prisma.io',
-          published: true,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Nilu',
-    email: 'nilu@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Follow Prisma on Twitter',
-          content: 'https://www.twitter.com/prisma',
-          published: true,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Mahmoud',
-    email: 'mahmoud@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Ask a question about Prisma on GitHub',
-          content: 'https://www.github.com/prisma/prisma/discussions',
-          published: true,
-        },
-        {
-          title: 'Prisma on YouTube',
-          content: 'https://pris.ly/youtube',
-        },
-      ],
-    },
-  },
-]
 const colorData : Prisma.ColorCreateInput[] = [
   {
     name:"Beyaz"
@@ -55,16 +10,24 @@ const colorData : Prisma.ColorCreateInput[] = [
      name:"Sarı"
   }
 ]
+
+const brandData : Prisma.BrandCreateInput[] = [
+  {
+    name:"Elegant Lixury",
+  },
+  {
+    name:"Elegant Clasic"
+  }
+]
 async function main() {
   console.log(`Start seeding ...`)
-  for (const u of userData) {
-    const user = await prisma.user.create({
-      data: u,
-    })
-    console.log(`Created user with id: ${user.id}`)
-  }
   for (const c of colorData) {
     const color = await prisma.color.create({
+      data: c
+    })
+  }
+  for (const c of brandData) {
+    const color = await prisma.brand.create({
       data: c
     })
   }
