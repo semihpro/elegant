f2339f2f8773b00cb4769488d298251020fc08e0
SemihCetin <semihcetin34@gmail.com>
2023-08-10 03:06:24 +0300

    versel postgre working thank youuuuu

--- prisma/temp_nosql/migrations/20230720115853_product_image_paths/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN "image_path1" TEXT;
-ALTER TABLE "Product" ADD COLUMN "image_path2" TEXT;
-ALTER TABLE "Product" ADD COLUMN "image_path3" TEXT;
-ALTER TABLE "Product" ADD COLUMN "image_path4" TEXT;
-ALTER TABLE "Product" ADD COLUMN "image_path5" TEXT;
