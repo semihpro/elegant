950f7c96c765435c1ea95884e8bd231c2764fae8
SemihCetin <semihcetin34@gmail.com>
2023-08-21 05:51:15 +0300

    projeler gallery admin

--- pages/admin/projeler/index.tsx
+++ pages/admin/projeler/index.tsx
@@ -1,30 +1,33 @@
 import { GetServerSideProps, NextPage } from "next";
-import axios from "axios";
-import path from "path";
+import Layout from "../../../components/Layout";
+import prisma from "../../../lib/prisma";
+import { Gallery } from "@prisma/client";
+import Breadcrumb from "../../../components/Breadcrumb";
+import { useState } from "react";
+import GalleryImageUpload from "../../../components/GalleryImageUpload";
 
-interface Props {
-  dirs: string[];
+export async function getServerSideProps(context) {
+  const gallery:Array<Gallery> = await prisma.gallery.findMany({});
+  return {
+    props:  {gallery} ,
+  };
 }
-
-const Home: NextPage<Props> = ({ dirs }) => {
-
+const Home: NextPage<{gallery:Gallery[]}> = ({gallery}) => {
   return (
-   <div>
-    <div className="table-responsive">
-      <table className="table">
-        <thead>
-          <tr>
-            <td>Aciklama</td>
-            <td>Link</td>
-            <td>Alt yazi</td>
-            <td>Icon</td>
-            <td>Icon Sirasi</td>
-            <td>Islemler</td>
-          </tr>
-        </thead>
-      </table>
-    </div>
-   </div>
+    <Layout>
+      <div className="page-title">
+      <Breadcrumb items={[{text:"Admin", link:"/admin"}, {text:"Gallery"}]}/>
+      </div>
+          <div className="form__group field">
+            <div className="grid-container">
+              {gallery.map(item=>{
+                return <GalleryImageUpload image={{id:item.id, path:(item.image_path ? item.image_path : `/no-image.jpg`), target:`image_path1`}}/>
+              })}
+              
+            </div>
+          </div>
+    </Layout>  
+  
   );
 };
 
