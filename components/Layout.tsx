ddc4a9a051fd96a123bbbdb3a33c76bbc2ee2f28
SemihCetin <semihcetin34@gmail.com>
2023-10-09 17:04:02 +0300

    suspence removed from project

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
-import React, { ReactNode, Suspense } from "react";
+import React, { ReactNode } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Loading from "./Loading";
+//import Loading from "./Loading";
 type Props = {
   children: ReactNode;
 };
@@ -10,9 +10,7 @@ const Layout: React.FC<Props> = (props) => (
   <div>
     <Header />
     <div className="layout">
-      <div className="container">
-        <Suspense fallback={<Loading />}>{props.children}</Suspense>
-      </div>
+      <div className="container">{props.children}</div>
     </div>
     <Footer />
   </div>
